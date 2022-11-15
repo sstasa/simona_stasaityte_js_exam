2403a3ae27e1df08a804ecbcb2d30d3a4830fcd7
Simona S <simona.stasaityte@gmail.com>
2022-11-15 09:47:07 +0200

    task 3 js done

--- Task 3/script.js
+++ Task 3/script.js
@@ -11,4 +11,38 @@ Pastaba: Sukurta kortelė, kurioje yra pateikiama vartotojo informacija, turi
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+const ENDPOINT = "https://api.github.com/users";
+const outputEl = document.getElementById("output");
+const showUsersBtn = document.getElementById("btn");
+
+let users = [];
+
+function getUsers(url) {
+  return fetch(url)
+    .then((resp) => resp.json())
+    .then((data) => data)
+    .catch((err) => console.warn("klaida getUsers", err));
+}
+
+async function mainInit() {
+  users = await getUsers(ENDPOINT);
+  console.log(`users = `, users);
+  outputEl.textContent = "";
+  outputEl.className = "outputGrid";
+  users.forEach((user) => {
+    createList(user);
+  });
+}
+
+showUsersBtn.addEventListener("click", mainInit);
+
+function createList(obj) {
+  const cardEl = document.createElement("div");
+  cardEl.className = "card";
+  const loginEl = document.createElement("h3");
+  loginEl.textContent = obj.login;
+  const imgEl = document.createElement("img");
+  imgEl.src = obj.avatar_url;
+  cardEl.append(loginEl, imgEl);
+  outputEl.append(cardEl);
+}
