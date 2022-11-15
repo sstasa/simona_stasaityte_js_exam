5ee130d8e7eec9ec4c2388304b58611d845e880a
Simona S <simona.stasaityte@gmail.com>
2022-11-15 10:33:46 +0200

    task 6 done

--- Task 6/script.js
+++ Task 6/script.js
@@ -9,13 +9,36 @@ atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann S
 -------------------------------------------------------------------------- */
 
 const users = [
-  { id: '1', name: 'John Smith', age: 20 },
-  { id: '2', name: 'Ann Smith', age: 24 },
-  { id: '3', name: 'Tom Jones', age: 31 },
-  { id: '4', name: 'Rose Peterson', age: 17 },
-  { id: '5', name: 'Alex John', age: 25 },
-  { id: '6', name: 'Ronald Jones', age: 63 },
-  { id: '7', name: 'Elton Smith', age: 16 },
-  { id: '8', name: 'Simon Peterson', age: 30 },
-  { id: '9', name: 'Daniel Cane', age: 51 },
+  { id: "1", name: "John Smith", age: 20 },
+  { id: "2", name: "Ann Smith", age: 24 },
+  { id: "3", name: "Tom Jones", age: 31 },
+  { id: "4", name: "Rose Peterson", age: 17 },
+  { id: "5", name: "Alex John", age: 25 },
+  { id: "6", name: "Ronald Jones", age: 63 },
+  { id: "7", name: "Elton Smith", age: 16 },
+  { id: "8", name: "Simon Peterson", age: 30 },
+  { id: "9", name: "Daniel Cane", age: 51 },
 ];
+
+function getUserAverageAge(arr) {
+  let sudetasAmzius = 0;
+  arr.forEach((obj) => {
+    sudetasAmzius += obj.age;
+  });
+  let vidurkis = (sudetasAmzius / arr.length).toFixed(0); //jei nereikejo toFixed tai ignoruokit :D
+  return vidurkis;
+}
+
+let usersVidurkis = getUserAverageAge(users);
+console.log(`usersVidurkis = `, usersVidurkis);
+
+function getUsersNames(arr) {
+  let names = [];
+  arr.forEach((obj) => {
+    names.push(obj.name);
+  });
+  return names;
+}
+
+let namesArr = getUsersNames(users);
+console.log(`namesArr = `, namesArr);
