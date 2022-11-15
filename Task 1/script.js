50c02bcc03b7c49ff8a39fb2697083a9a6d500ef
Simona S <simona.stasaityte@gmail.com>
2022-11-15 16:31:35 +0200

    fixed task 3 responsiveness

--- Task 1/script.js
+++ Task 1/script.js
@@ -12,20 +12,36 @@ const calcBtnEl = document.getElementById("submit-btn");
 const kgInputEl = document.getElementById("search");
 
 function convertWeight(kg) {
+  outputEl.textContent = "";
   let convertToLb = kg * 2.2046;
   let convertToG = kg / 0.001; //prettieris nuima galinius nulius nuo sito, bet nemanau kad tai problema
   let convertToOz = kg * 35.274;
   console.log(`convertToLb = `, convertToLb);
   console.log(`convertToG = `, convertToG);
   console.log(`convertToOz = `, convertToOz);
-  outputEl.textContent = `${kg}kg = ${convertToLb}lb | ${kg}kg = ${convertToG}g | ${kg}kg = ${convertToOz}oz`;
+  //creating html elements
+  const kgEl = document.createElement("h2");
+  kgEl.textContent = `${kg}kg =`;
+  const convertedEl = document.createElement("div");
+  const lbEl = document.createElement("p");
+  lbEl.textContent = `${convertToLb} lb`;
+  const gEl = document.createElement("p");
+  gEl.textContent = `${convertToG} g`;
+  const ozEl = document.createElement("p");
+  ozEl.textContent = `${convertToOz} oz`;
+  outputEl.append(kgEl, convertedEl);
+  convertedEl.append(lbEl, gEl, ozEl);
 }
 
 calcBtnEl.addEventListener("click", (event) => {
   event.preventDefault();
+  if (search.value > 1e29) {
+    outputEl.textContent = "Per didelis skaičius!";
+    return;
+  }
   switch (search.value > 0) {
     case false:
-      outputEl.textContent = "Įveskite svorį!";
+      outputEl.textContent = "Įveskite svorį (be raidžių)!";
       break;
     case true:
       convertWeight(search.value);
