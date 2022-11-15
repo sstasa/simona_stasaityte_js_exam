d83834da398b82a76cf11343367b47288bed1e3c
Simona S <simona.stasaityte@gmail.com>
2022-11-15 10:13:07 +0200

    task 4 js done

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,4 +8,38 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = "cars.json";
+const outputEl = document.getElementById("output");
+let cars = [];
+
+function getCars(from) {
+  return fetch(from)
+    .then((resp) => resp.json())
+    .then((data) => data)
+    .catch((err) => console.warn("klaida getCars", err));
+}
+
+async function mainInit() {
+  cars = await getCars(ENDPOINT);
+  console.log(`cars = `, cars);
+  cars.forEach((manufacturer) => {
+    displayCars(manufacturer);
+  });
+}
+
+mainInit();
+
+function displayCars(obj) {
+  const cardEl = document.createElement("div");
+  cardEl.classList = "card";
+  const brandEl = document.createElement("h3");
+  brandEl.textContent = obj.brand;
+  const modelsArr = obj.models;
+  cardEl.append(brandEl);
+  modelsArr.forEach((model) => {
+    const modelsEl = document.createElement("li");
+    modelsEl.textContent += model;
+    cardEl.append(modelsEl);
+  });
+  outputEl.append(cardEl);
+}
